cef29c2e38a3a71fb3f68238ae753ff7afd26113
myx <john@smith.kyon>
2020-05-19 14:39:09 +0800

    chore: 生产环境压缩配置

--- client/config/prod.js
+++ client/config/prod.js
@@ -5,6 +5,11 @@ module.exports = {
   defineConstants: {
   },
   mini: {},
+  uglify: {
+    enable: true,
+    config: {
+    }
+  },
   h5: {
     /**
      * 如果h5端编译后体积过大，可以使用webpack-bundle-analyzer插件对打包体积进行分析。
