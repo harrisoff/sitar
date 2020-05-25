e6f1a934ccffb943248d2bb63bb16ba90d13b6d2
myx <john@smith.kyon>
2020-05-25 10:37:04 +0800

    fix: 修正日志上传逻辑

--- client/src/utils/Logger.js
+++ client/src/utils/Logger.js
@@ -20,24 +20,23 @@ class Logger {
   }
   _upload() {
     if (this._isUploading || this._timer || !this._logs.length) return;
-    // 记录回滚位置
-    const currentLen = this._logs.length;
+    // 把要上传的部分切出来，用来回滚
+    const fragment = this._logs.splice(0, this._maxLen)
     this._isUploading = true;
-    uploadLogs(this._logs)
-      .then(_ => {
-        // 上传成功后删除
-        this._logs = this._logs.splice(0, currentLen);
-      })
+    uploadLogs(fragment)
+      .then()
       .catch(err => {
+        // 上传失败，合并回来
+        this._logs = fragment.concat(this._logs)
         console.error(err);
-        // 如果上传失败，暂时阻止上传
-        // _delay 后恢复
+        // 如果上传失败，暂时阻止上传，_delay 后恢复
         this._timer = setTimeout(() => {
           this._timer = null;
         }, this._delay);
+        // 记录
         this.addError("logger", err);
       })
-      .then(_ => {
+      .then(() => {
         this._isUploading = false;
       });
   }
