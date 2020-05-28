66b51d2b89faa7fb98cb666bbf91bb86918238a4
myx <john@smith.kyon>
2020-05-28 16:09:23 +0800

    fix: 完善关于页面

--- client/src/pages/about/index.jsx
+++ client/src/pages/about/index.jsx
@@ -1,9 +1,10 @@
 import Taro from "@tarojs/taro";
 import { observer, inject } from "@tarojs/mobx";
-import { View } from "@tarojs/components";
+import { View, Image } from "@tarojs/components";
 import { AtMessage } from "taro-ui";
 
 import { formatTime } from "../../utils/weapp";
+import { CDN } from '../../config'
 
 import BaseComponent from "../../components/Base.jsx";
 
@@ -21,12 +22,26 @@ export default class Index extends BaseComponent {
     return (
       <View className='page-about'>
         <AtMessage />
-        <View>简介</View>
-        <View>
+        <View style='text-align: center'>
+          <Image src={`${CDN.PREFIX1}/welcome.jpg`} style='height: 200rpx' mode='aspectFit' />
+        </View>
+        <View className='section intro'>
+          <View className='section__title'>简介</View>
+          <View className='section__content'>
+            <View className='intro__content__p'>
+              1. 文章、图片均来自同名公众号【西塔尔之声】
+            </View>
+            <View className='intro__content__p'>
+              2. 小程序不修改内容，仅提供分类、搜索、评论等功能
+            </View>
+            <View className='intro__content__p'>
+              3. 不能保证展示效果与公众号完全一致，比如小程序中无法使用超链接
+            </View>
+          </View>
+        </View>
+        <View className='section last-update'>
           内容最后更新于{formatTime(this.props.cacheStore.version, true)}
         </View>
-        <View>小程序版本号 v0.0.1-beta</View>
-        <View>更新日志</View>
       </View>
     );
   }
